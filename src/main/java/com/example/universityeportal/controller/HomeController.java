59a47cb4ca7996e9e7950985572668126ca0e6da
Cyrus <cyruslarkpor.geradliberia@gmail.com>
2026-05-01 17:36:50 +0000

    Create unified login page with same design for all portals using Image 1.jpg background

--- src/main/java/com/example/universityeportal/controller/HomeController.java
+++ src/main/java/com/example/universityeportal/controller/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return "unified-login";
     }
 
     @GetMapping("/forgot-password")
