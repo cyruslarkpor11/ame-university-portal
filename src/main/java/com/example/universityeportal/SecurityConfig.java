a6d661f08ab48f856ff758284e807af196925d6e
Cyrus <cyruslarkpor.geradliberia@gmail.com>
2026-05-01 00:09:33 +0000

    Remove unused UserRepository import from SecurityConfig.java

--- src/main/java/com/example/universityeportal/SecurityConfig.java
+++ src/main/java/com/example/universityeportal/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.universityeportal;
 
-import com.example.universityeportal.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
